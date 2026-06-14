01445918e0330ee94d359a24759623990c7c2525
jirin123men <muhajirinrt@gmail.com>
2026-06-14 21:31:58 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBibUYRx-eti666t0P5B4xpgSU7WyRQPZ4",
+  authDomain: "mybengkelku.firebaseapp.com",
+  projectId: "mybengkelku",
+  storageBucket: "mybengkelku.firebasestorage.app",
+  messagingSenderId: "1041434407055",
+  appId: "1:1041434407055:web:008ee7323d84ae70cf00fa"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
